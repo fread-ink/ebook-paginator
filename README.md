0b5ccd16644bd878cc61d81fe75f1aee6a18980e
Marc Juul <marc@juul.io>
2020-01-25 00:54:51 -0800

    basics are working

--- README.md
+++ README.md
@@ -1,26 +1,22 @@
 
 An example of how to cleanly break an HTML document into pages without having the last line on each page potentially cut off as usually happens with scrolling.
 
-Uses an `<iframe>` to show an html document and injects the CSS rules `column-width`, `column-gap` and `height` for the `<body>` element inside the document contained in the iframe.
+Uses an `<iframe>` to show an html document then traverses the html elements in the document inside the iframe to figure out what's being cut off at the bottom, then hides whatever is being cut off.
 
-Pressing `space` progresses to the next page.
+This is an alternative to the CSS `column` based solution in the main branch of this repo. This solution takes quite a bit longer to calculate the location of the page breaks (which is no surprise since here it's done in javascript vs. the browser built-in functionality) but the advantage is that it can be done asynchronously while the user reads. The downside is that calculating the total number of pages in a book takes a fairly long time, as does jumping to a specific page. This work _could_ be performed in the background whenever a new book is added to the user's library but then adding a lot of books could take a serious chunk out of battery life. It also doesn't make a ton of sense to pre-calculate this given that e.g. changing font size or screen dimensions (e.g. flipping between vertical and horizontal reading modes) will require this to be re-calculated.
 
-A demo is [hosted here](https://juul.io/paginator/). Open the console to see the timing measurement.
+Here are the calculation times for various browsers:
 
-This example demonstrates an issue with rendering time for column layouts on long documents.
+* Chromium 78: 5.4 seconds (0.3 synchronous)
+* Firefox 71: 9.7 seconds (3.1 synchronous) (might be slowed by extensions)
+* Epiphany (WebKit2GTK): 5.4 seconds (0.4 synchronous)
 
-Here are rendering times for various browsers:
+A demo is [hosted here](https://juul.io/paginator-alt/). Open the console to see the timing measurement.
 
-* Chromium 78: 0.60 seconds
-* Firefox 71: 1.47 seconds
-* Epiphany (WebKit2GTK): 14.54 seconds
+Pressing `space` progresses to the next page by insta-scrolling to the top of the element that was cut off at the bottom. Pressing `t` to start an asynchronous time test for repeating the "where to cut off"-calculation for a page 1000 times. You can also press `s` to start a synchronous version of the test. This will complete faster but make the browser tab unresponsive while it's running.
 
-These numbers are only accounting for time taking from when the CSS column rules were added and the iframe resized to when the changes show up on screen. The numbers are from an Intel i5-2520 running at 2.5 GHz. The numbers are for rendering 402 pages (page number will depend on browser window width). The browsers all freeze at least the current tab during this time, showing a blank iframe, and the user is unable to navigate pages during rendering. 
+This example requires that you have permissions to access the document inside the iframe from javascript in the main document.
 
-This example requires that you have permissions to modify the document inside the iframe from javascript in the main document, or the ability to inject CSS rules into the document inside the iframe using some other method.
-
-You need to host this example on a web server since browsers consider two files loaded from the filesystem to be from different domains and does not allow injection of the CSS rules into the iframe document.
-
-Works in Firefox 71.0. Pixel offsets are somewhat off on WebKit (Epiphany) and Chromium but that should be easy to fix.
+You need to host this example on a web server since browsers consider two files loaded from the filesystem to be from different domains and thus does not allow accessing the iframe document.
 
 Currently doesn't take into account page resizing, scrolling or font size changes, so reload after changing any of that.
--- script.js
+++ script.js
@@ -6,11 +6,17 @@ class Paginator {
 
     this.iframe = document.getElementById('iframe');
     this.iframeContainer = document.getElementById('iframe-container');
+    this.bottomHider = document.getElementById('bottom-hider');
         
     this.iframe.addEventListener('load', this.onIframeLoad.bind(this));
     this.timeStart = Date.now();
     this.iframe.src = iframeURI;
 
+    // the scrollY values for each page in the document
+    this.pageScroll = [0];
+    this.curPage = 0;
+
+    this.speedTestBound = this.speedTest.bind(this);
   }
 
 
@@ -23,26 +29,10 @@ class Paginator {
 
     document.body.addEventListener('keypress', this.onKeyPress.bind(this));
     this.iDoc.addEventListener('keypress', this.onKeyPress.bind(this));
-    
-    const timeStart = Date.now();
 
-    // Modify iframe document body style to use column layout
-    this.iDoc.body.style.columnWidth = this.iframeContainer.offsetWidth + 'px';
-    this.iDoc.body.style.columnGap = '60px';
-    this.iDoc.body.style.height = this.iframeContainer.offsetHeight + 'px';
+    this.walker = this.iDoc.createTreeWalker(this.iDoc, NodeFilter.SHOW_TEXT); 
+    this.hideHalfShownLines();
 
-    // Get the width that the <body> document inside the iframe
-    // _wants_ to have and resize the iframe to make the <body> fit.
-    // This will result in an extremely width iframe.
-    const range = document.createRange();
-    range.selectNodeContents(this.iDoc.body);
-    this.iframe.style.width = range.getBoundingClientRect().width + 100 + 'px'
-
-    // This line causes the browser to wait for the change to be applied
-    // before running the console.log
-    const tmp = this.iDoc.body.offsetWidth;
-    console.log("Rendering took:", (Date.now() - timeStart) / 1000, "seconds", );
-    console.log("for", this.countPages(), "pages");
   }
 
   countPages() {
@@ -52,16 +42,205 @@ class Paginator {
   }
 
   nextPage() {
-    const curLeft = this.iframe.offsetLeft;
-    this.iframe.style.left = (curLeft - this.iframeContainer.offsetWidth - 61) + 'px';
+    const nextPageStart = this.pageScroll[this.curPage+1];
+//    console.log("next page:", nextPageStart)
+    this.iWin.scrollTo(0, nextPageStart);
+    this.hideHalfShownLines()
+    this.curPage++;
   }
 
-  onKeyPress(e) {  
+  // This is quite a bit faster as a loop (instead of recursive
+  speedTest(numPages, count) {
+    if(!numPages) numPages = 1000;
+    if(!count) {
+      count = 0;
+      console.log("Starting async speed test for", numPages, "pages");
+      this.t = Date.now();
+    }
+
+
+    if(count >= numPages) {
+      console.log("Took:", (Date.now() - this.t) / 1000, 'seconds')
+      return;
+    }
+
+    this.hideHalfShownLines(true, this.speedTestBound, numPages, count);
+  }
+
+  speedTestLoop(numPages) {
+    if(!numPages) numPages = 1000;
+
+    console.log("Starting sync speed test for", numPages, "pages");
+    const t = Date.now();
+    
+    var i;
+    for(i=0; i < numPages; i++) {
+      this.hideHalfShownLines(true);
+    }
+    console.log("Took:", (Date.now() - t) / 1000, 'seconds')
+  }
+  
+  isPartiallyInView(el) {
+
+    var rect;
+    if(el.getBoundingClientRect) {
+      rect = el.getBoundingClientRect();
+
+    } else {
+      const range = document.createRange();
+      range.selectNodeContents(el);
+      rect = range.getBoundingClientRect();
+    }
+    const bottom = document.getElementById('iframe').offsetHeight;
+
+    if(rect.top < bottom && rect.bottom > bottom) {
+//      console.log("Debug:", bottom, rect.top, rect.bottom, el);
+      document.getElementById('debugger').style.height = (rect.top) + 'px';
+      return 0;
+    } else if(rect.top > bottom) {
+      return 1;
+    } else {
+      return -1
+    }
+  }
+
+  hideHalfShownLines(rewind, cb, total, count) {
+
+    var toRewind = 0;
+    
+    var i, cur, found, bottomCutoff, diff, nextOffset;
+
+    for(i=0; i < 2000; i++) { // TODO find real max
+      cur = this.walker.currentNode;
+      if(cur.nodeName !== '#text') {
+        this.walker.nextNode();
+        toRewind++;
+        continue;
+      }
+      const inView = this.isPartiallyInView(cur);
+      if(inView == 0) {
+//        console.log("Node:", cur);
+        const range = document.createRange();
+        range.selectNodeContents(cur);
+        var rect;
+        var lastTop = -1;
+        var tooFar = false;
+        var lastTooFar = false;
+        var lastMove = 0;
+//        console.log('------------------------');
+        // TODO what if there is only a single character?
+        while(true) {
+//          console.log("at:", range.startOffset, '-', range.endOffset);          
+          diff = range.endOffset - range.startOffset;
+          rect = range.getBoundingClientRect();
+          
+          // Use binary search to find the correct cut-off point
+
+          if(rect.top > (this.iframe.offsetHeight)) {
+
+//            console.log('FOUND!', rect.top, this.iframe.offsetHeight);
+
+//            bottomCutoff = (this.iframe.offsetHeight - lastTop) - 1;            
+//            found = true;
+//            break;
+
+            tooFar = true;
+          } else {
+            tooFar = false;
+            lastTop = rect.top;
+          }
+          lastTooFar = tooFar;
+
+//          console.log("  diff:", diff, tooFar);
+
+          if(lastMove == 1) {
+//            console.log("Ended on:", range.startOffset, tooFar, lastTooFar, '-', rect.top, lastTop);
+            break;
+          } else if(range.startOffset == 0) {
+            nextOffset = Math.round(range.endOffset / 2);
+          } else {
+            if(tooFar) {
+              if(range.lastMove < 4) {
+//                console.log("too far (close to beginning)", range.startOffset, '-', rect.top, lastTop);
+                nextOffset = range.startOffset - (lastMove - 1);
+              } else {
+//                console.log("too far (long way to go)", range.startOffset, '-', rect.top, lastTop);
+                nextOffset = range.startOffset - Math.round(lastMove / 2) - 1;
+              }
+            } else {
+              if(lastMove < 4) {
+//                console.log("NOT too far (close to end)", range.startOffset, '-', rect.top, lastTop);
+                nextOffset = range.startOffset + (lastMove - 1);
+              } else {
+//                console.log("NOT too far (long way to go)", range.startOffset, '-', rect.top, lastTop);
+                nextOffset = range.startOffset + Math.round(lastMove / 2) + 1;
+              }
+            }
+          }
+          if(nextOffset < 0) {
+            nextOffset = 0;
+          } else if(nextOffset > range.endOffset) {
+            nextOffset = range.endOffset;
+          }
+          lastMove = Math.abs(range.startOffset - nextOffset);
+          range.setStart(cur, nextOffset);
+        }
+
+        if(lastTop > -1) {
+          bottomCutoff = Math.round((this.iframe.offsetHeight - lastTop) - 1);
+          if(bottomCutoff < 1) {
+            bottomCutoff = 0;
+          }
+//          console.log("Cutting off:", bottomCutoff);
+        }
+        found = true;
+        break;
+      }
+
+      this.walker.nextNode();
+      toRewind++;
+
+      // We went too far
+      if(inView == 1) {
+        this.walker.previousNode();
+        if(toRewind) {
+          toRewind--;
+        }
+        break;
+      }
+    }
+    if(!found) {
+//      console.log("NOTHING");
+      bottomCutoff = 0;
+    }
+    
+    const nextPageScroll = this.iWin.scrollY + this.iframeContainer.offsetHeight - bottomCutoff;
+    if(!rewind) {
+      this.pageScroll.push(nextPageScroll);
+      this.bottomHider.style.height = bottomCutoff + 'px';
+    }
+    
+    for(i=0; i < toRewind; i++) {
+      this.walker.previousNode();
+    }
+    if(cb) setTimeout(cb, 0, total, count+1)
+  }
+  
+  onKeyPress(e) {
+    
     switch(e.charCode) {
       
     case 32: // space
       this.nextPage();
       break;
+    case 116: // t
+      this.speedTest();
+      break;
+    case 115: // s
+      this.speedTestLoop();
+      break;
+    case 98: // 'b'
+      this.iWin.scrollTo(0, this.iWin.scrollY - 10);
     }
   }
 
