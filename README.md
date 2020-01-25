0fc96fab0a82dc1bd06e87443816b55c4ccde561
Marc Juul <marc@juul.io>
2020-01-25 02:37:10 -0800

    re-worked code to be able to pre-calculate all page boundaries

--- README.md
+++ README.md
@@ -5,15 +5,15 @@ Uses an `<iframe>` to show an html document then traverses the html elements in 
 
 This is an alternative to the CSS `column` based solution in the main branch of this repo. This solution takes quite a bit longer to calculate the location of the page breaks (which is no surprise since here it's done in javascript vs. the browser built-in functionality) but the advantage is that it can be done asynchronously while the user reads. The downside is that calculating the total number of pages in a book takes a fairly long time, as does jumping to a specific page. This work _could_ be performed in the background whenever a new book is added to the user's library but then adding a lot of books could take a serious chunk out of battery life. It also doesn't make a ton of sense to pre-calculate this given that e.g. changing font size or screen dimensions (e.g. flipping between vertical and horizontal reading modes) will require this to be re-calculated.
 
-Here are the calculation times for various browsers:
+Here are the calculation times for a 1000 page document for various browsers:
 
-* Chromium 78: 5.4 seconds (0.3 synchronous)
+* Chromium 78: 0.24 synchronous)
+* Epiphany (WebKit2GTK): (0.3 synchronous)
 * Firefox 71: 9.7 seconds (3.1 synchronous) (might be slowed by extensions)
-* Epiphany (WebKit2GTK): 5.4 seconds (0.4 synchronous)
 
-A demo is [hosted here](https://juul.io/paginator-alt/). Open the console to see the timing measurement.
+A demo is [hosted here](https://juul.io/paginator-alt/).
 
-Pressing `space` progresses to the next page by insta-scrolling to the top of the element that was cut off at the bottom. Pressing `t` to start an asynchronous time test for repeating the "where to cut off"-calculation for a page 1000 times. You can also press `s` to start a synchronous version of the test. This will complete faster but make the browser tab unresponsive while it's running.
+Pressing `space` progresses to the next page by insta-scrolling to the top of the element that was cut off at the bottom. You can also press `s` to start a synchronous test to calculate the page boundaries. Open the console to see the timing measurement.
 
 This example requires that you have permissions to access the document inside the iframe from javascript in the main document.
 
--- script.js
+++ script.js
@@ -31,7 +31,7 @@ class Paginator {
     this.iDoc.addEventListener('keypress', this.onKeyPress.bind(this));
 
     this.walker = this.iDoc.createTreeWalker(this.iDoc, NodeFilter.SHOW_TEXT); 
-    this.hideHalfShownLines();
+    this.hideHalfShownLines2();
 
   }
 
@@ -49,9 +49,32 @@ class Paginator {
     this.curPage++;
   }
 
+  hideHalfShownLines2() {
+    const val = this.findPageEnd(this.walker);
+//    console.log("Got:", val);
+
+    var bottomCutoff;
+    if(val < 0) {
+      bottomCutoff = 0;
+      this.pageScroll[this.curPage+1] = this.iWin.scrollY + this.iframe.offsetHeight;
+    } else {
+      bottomCutoff = (this.iframe.offsetHeight - val);
+      this.pageScroll[this.curPage+1] = this.iWin.scrollY + val;
+    }
+    this.bottomHider.style.height = bottomCutoff + 'px';
+
+    return bottomCutoff;
+  }
+  
+  nextPage2() {
+    this.iWin.scrollTo(0, this.pageScroll[++this.curPage]);
+    const bottomCutoff = this.hideHalfShownLines2();
+
+  }
+  
   // This is quite a bit faster as a loop (instead of recursive
   speedTest(numPages, count) {
-    if(!numPages) numPages = 1000;
+    if(!numPages) numPages = 300;
     if(!count) {
       count = 0;
       console.log("Starting async speed test for", numPages, "pages");
@@ -67,6 +90,39 @@ class Paginator {
     this.hideHalfShownLines(true, this.speedTestBound, numPages, count);
   }
 
+  speedTestLoop2(numPages) {
+    if(!numPages) numPages = 280;
+
+    const pageHeight = this.iframe.offsetHeight;
+    const walker = this.iDoc.createTreeWalker(this.iDoc, NodeFilter.SHOW_TEXT);
+//    const findPageEnd = this.findPageEnd;
+    
+    console.log("Starting sync2 speed test for", numPages, "pages");
+    const t = Date.now();
+
+    const pages = [];
+    var offset = 0;
+    var i, val;
+    for(i=0; i < numPages; i++) {
+      val = this.findPageEnd(walker, offset);
+//      console.log("###### ", pages.length, '=', offset, '|', val, );
+      pages.push(offset);
+      if(val < 0) {
+        offset = offset + pageHeight;
+      } else {
+        offset = val + 1;
+      }
+    }
+    console.log("Took:", (Date.now() - t) / 1000, 'seconds')
+    //    console.log(pages);
+    const toPage = 999;
+
+    this.curPage = toPage;
+    this.iWin.scrollTo(0, pages[toPage]);
+    this.hideHalfShownLines2();
+    
+  }
+  
   speedTestLoop(numPages) {
     if(!numPages) numPages = 1000;
 
@@ -80,22 +136,23 @@ class Paginator {
     console.log("Took:", (Date.now() - t) / 1000, 'seconds')
   }
   
-  isPartiallyInView(el) {
-
+  isPartiallyInView(el, offset) {
+    offset = offset || 0;
     var rect;
     if(el.getBoundingClientRect) {
       rect = el.getBoundingClientRect();
-
+      // TODO handle non-text elements
+      return -1;
     } else {
-      const range = document.createRange();
+      let range = document.createRange();
       range.selectNodeContents(el);
       rect = range.getBoundingClientRect();
     }
-    const bottom = document.getElementById('iframe').offsetHeight;
-
+    const bottom = this.iframe.offsetHeight + offset;
+    
+//    console.log("Debug:", bottom, rect.top, rect.bottom);
     if(rect.top < bottom && rect.bottom > bottom) {
-//      console.log("Debug:", bottom, rect.top, rect.bottom, el);
-      document.getElementById('debugger').style.height = (rect.top) + 'px';
+//      document.getElementById('debugger').style.height = (rect.top) + 'px';
       return 0;
     } else if(rect.top > bottom) {
       return 1;
@@ -104,8 +161,118 @@ class Paginator {
     }
   }
 
+
+  findPageEnd(walker, offset) {
+    offset = offset || 0;
+    const range = document.createRange();
+    
+    var cur, inView, found, diff, nextOffset, rect, lastTop, tooFar, lastTooFar, lastMove;
+
+    while(true) {
+      cur = walker.currentNode;
+      if(cur.nodeName !== '#text') {
+        if(!walker.nextNode()) break;
+        continue;
+      }
+      
+      inView = this.isPartiallyInView(cur, offset);
+
+      // We didn't go far enough
+      if(inView < 0) {
+//        console.log("not far enough", cur);
+        if(!walker.nextNode()) break;
+        continue;
+      }
+      
+      // We went too far
+      if(inView > 0) {
+//        console.log("too far", cur);
+        walker.previousNode();
+        break;
+      }
+
+      range.selectNodeContents(cur);
+
+      
+      var lastTop = -1;
+      tooFar = false;
+      lastTooFar = false;
+      lastMove = 0;
+      
+      //        console.log('------------------------');
+      // TODO what if there is only a single character?
+      while(true) {
+        //          console.log("at:", range.startOffset, '-', range.endOffset);          
+        diff = range.endOffset - range.startOffset;
+        rect = range.getBoundingClientRect();
+        
+        // Use binary search to find the correct cut-off point
+
+        if(rect.top > (this.iframe.offsetHeight + offset)) {
+
+          //            console.log('FOUND!', rect.top, this.iframe.offsetHeight);
+
+          //            bottomCutoff = (this.iframe.offsetHeight - lastTop) - 1;            
+          //            found = true;
+          //            break;
+
+          tooFar = true;
+        } else {
+          tooFar = false;
+          lastTop = rect.top;
+        }
+        lastTooFar = tooFar;
+
+        //          console.log("  diff:", diff, tooFar);
+
+        if(lastMove == 1) {
+          //            console.log("Ended on:", range.startOffset, tooFar, lastTooFar, '-', rect.top, lastTop);
+          break;
+        } else if(range.startOffset == 0) {
+          nextOffset = Math.round(range.endOffset / 2);
+        } else {
+          if(tooFar) {
+            if(range.lastMove < 4) {
+              //                console.log("too far (close to beginning)", range.startOffset, '-', rect.top, lastTop);
+              nextOffset = range.startOffset - (lastMove - 1);
+            } else {
+              //                console.log("too far (long way to go)", range.startOffset, '-', rect.top, lastTop);
+              nextOffset = range.startOffset - Math.round(lastMove / 2) - 1;
+            }
+          } else {
+            if(lastMove < 4) {
+              //                console.log("NOT too far (close to end)", range.startOffset, '-', rect.top, lastTop);
+              nextOffset = range.startOffset + (lastMove - 1);
+            } else {
+              //                console.log("NOT too far (long way to go)", range.startOffset, '-', rect.top, lastTop);
+              nextOffset = range.startOffset + Math.round(lastMove / 2) + 1;
+            }
+          }
+        }
+        if(nextOffset < 0) {
+          nextOffset = 0;
+        } else if(nextOffset > range.endOffset) {
+          nextOffset = range.endOffset;
+        }
+        lastMove = Math.abs(range.startOffset - nextOffset);
+        range.setStart(cur, nextOffset);
+      }
+
+      if(lastTop > -1) {
+        walker.nextNode();
+        return Math.round(lastTop - 1);
+      }
+
+      if(!walker.nextNode()) break;
+    }
+
+    return -1;
+  }
+
+  
   hideHalfShownLines(rewind, cb, total, count) {
 
+    const range = document.createRange();
     var toRewind = 0;
     
     var i, cur, found, bottomCutoff, diff, nextOffset;
@@ -120,7 +287,7 @@ class Paginator {
       const inView = this.isPartiallyInView(cur);
       if(inView == 0) {
 //        console.log("Node:", cur);
-        const range = document.createRange();
+
         range.selectNodeContents(cur);
         var rect;
         var lastTop = -1;
@@ -237,8 +404,12 @@ class Paginator {
       this.speedTest();
       break;
     case 115: // s
-      this.speedTestLoop();
+      this.speedTestLoop2();
       break;
+    case 113: // q
+      this.nextPage2();
+      break;
+      
     case 98: // 'b'
       this.iWin.scrollTo(0, this.iWin.scrollY - 10);
     }
